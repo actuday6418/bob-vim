d148a25f53b896084dddb26fc5e15736d7b79f2e
Arun Sojan Parolikkal <actuday8456@gmail.com>
2020-07-11 00:00:44 +0530

    Initial

---
+++ ftdetect/bob.vim
@@ -0,0 +1,1 @@
+au BufNewFile,BufRead *.bob set filetype=bob
---
+++ pathogen_installer.sh
@@ -0,0 +1,5 @@
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+touch ~/.vimrc
+echo 'execute pathogen#infect()' > ~/.vimrc
+
+
